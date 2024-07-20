b1ca76725e9d5bb4f7c4c5429ed09c515efca682
blindcinema <jannemetguzvic@gmail.com>
2024-07-20 17:53:49 +0200

    Revert "nextjsconfig"
    This reverts commit a29338e94c3a05ddfaba2c77e3d2aa6ee55e1e64.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = {};
+module.exports = {
     typescript: {
-        ignoreBuildErrors: true,
-      },
-
-};
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
+  }
 export default nextConfig;
